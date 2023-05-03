7c71718662eebc36e6ee16d11b7561543e8c8a0a
Megha123-sys <meghaggerwal5396@gmail.com>
2023-05-03 17:12:07 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import customtkinter
+import openai
+import os
+import pickle
+
+app = customtkinter.CTk()
+app.title("chatGPT bot")
+app.geometry("750x600")
+
+app.iconbitmap("C:/Users/Megha Aggarwal/Downloads/bot/ai_lt.ico")
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+app.mainloop()
